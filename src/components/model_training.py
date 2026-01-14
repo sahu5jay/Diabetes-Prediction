00bde44022c71c1bfe73c1f0f044e7719bf89d97
JaySahu <jsahu0523@gmail.comm>
2026-01-15 02:26:44 +0530

    Preprocessor model save in pickel file

--- src/components/model_training.py
+++ src/components/model_training.py
@@ -1,5 +1,71 @@
 import sys
+import pandas as pd
 import os 
 from src.exception import CustomException
 from src.logger import logging
-from
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+# from src.components.Data_transformation import DataTransformation
+
+from dataclasses import dataclass
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def iniate_model_training(self, train_array, test_array):
+
+        try:
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            models = {
+                "Logistic Regression": LogisticRegression(max_iter=1000),
+                "Decision Tree": DecisionTreeClassifier(),
+                "Random Forest": RandomForestClassifier(),
+                "Support Vector Machine": SVC(),
+                "K-Nearest Neighbors": KNeighborsClassifier()
+            }
+
+            param_grids = {
+                "Logistic Regression": {
+                    "C": [0.01, 0.1, 1, 10],
+                    "solver": ["liblinear"]
+                },
+                "Decision Tree": {
+                    "max_depth": [None, 5, 10, 20],
+                    "min_samples_split": [2, 5, 10]
+                },
+                "Random Forest": {
+                    "n_estimators": [50, 100, 200],
+                    "max_depth": [None, 10, 20]
+                },
+                "Support Vector Machine": {
+                    "C": [0.1, 1, 10],
+                    "kernel": ["linear", "rbf"]
+                },
+                "KNN": {
+                    "n_neighbors": [3, 5, 7, 9],
+                    "weights": ["uniform", "distance"]
+                }
+            }
+
+        
+        except Exception as e:
+            logging.info("Exception accured  while training the model")
+            raise CustomException(e,sys)
+        
+
+
+
